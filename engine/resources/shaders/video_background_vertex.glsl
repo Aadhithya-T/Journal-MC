a1f0bdf7b60186c8233c93001e89adb4ad0a056f
Aadhithya-T <aadhithyathirumalai2314@gmail.com>
2026-08-18 23:02:42 +0530

    fix(video): correct vertical UV orientation for video background rendering

--- engine/resources/shaders/video_background_vertex.glsl
+++ engine/resources/shaders/video_background_vertex.glsl
@@ -4,6 +4,7 @@ layout (location = 0) in vec2 aPos;
 out vec2 vUv;
 
 void main() {
-    vUv = (aPos + 1.0) * 0.5;
+    // Correct upright UV orientation (top-left is (0, 0), bottom-right is (1, 1))
+    vUv = vec2((aPos.x + 1.0) * 0.5, 1.0 - (aPos.y + 1.0) * 0.5);
     gl_Position = vec4(aPos, 0.0, 1.0);
 }
